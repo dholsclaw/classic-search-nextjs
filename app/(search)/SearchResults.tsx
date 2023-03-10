83df7e5deed0abeac0365ef846c157c3555edf1a
Dave Holsclaw <holsclaw@mac.com>
2023-03-10 08:16:39 -0600

    add key for search results component

--- app/(search)/SearchResults.tsx
+++ app/(search)/SearchResults.tsx
@@ -9,7 +9,7 @@ interface SearhResultsProps {
 function SearchResults({results}: SearhResultsProps) {
     return (
         <div className="w-full bg-white flex flex-col shadow rounded mt-4 max-h-48 overflow-scroll">
-            {results.map((result: User) => <SearchResult result={result}/>)}
+            {results.map((result: User) => <SearchResult key={result.id} result={result}/>)}
         </div>
     );
 }
